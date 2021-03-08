3cfa43012074fd8fbadd66c628fce721539e95b9
jxmeslukin <jimmy.lukin@gmail.com>
2021-03-08 14:19:07 +1100

    Initialize dependabot/npm_and_yarn/ini-1.3.8

---
+++ src/definitions/images.ts
@@ -0,0 +1,26 @@
+declare module '*.gif' {
+  const value: string;
+  export = value;
+}
+declare module '*.jpg' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export = value;
+}
+declare module '*.png' {
+  const value: string;
+  export = value;
+}
+declare module '*.svg' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.wbep' {
+  const value: string;
+  export = value;
+}
